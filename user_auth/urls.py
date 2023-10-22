3c03fa32a454a43de429fe32d77f976fefe57fca
xrisent <ormonov31261@gmail.com>
2023-10-23 02:09:49 +0600

    i made JWT tokens and forgot to commit it

--- user_auth/urls.py
+++ user_auth/urls.py
@@ -16,7 +16,7 @@ router.register(r'person', views.PersonViewSet)
 urlpatterns = [
     path('', include(router.urls)),
 
-    # jwt tokens
+    # JWT tokens
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
